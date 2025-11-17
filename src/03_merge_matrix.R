953dabd7dd6e717a1f59211d2dd6f0e83f60f625
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-18 01:16:01 +0800

    Update 03_merge_matrix.R

--- src/03_merge_matrix.R
+++ src/03_merge_matrix.R
@@ -2,3 +2,19 @@ require(GCModeller);
 
 imports "geneExpression" from "phenotype_kit";
 
+let data_dir = relative_work("../data/kmer_matrix");
+let box_matrix = relative_work("../data/box_matrix.csv");
+let load_kmers = list.files(data_dir, pattern = "*.csv");
+
+print(basename(load_kmers));
+
+load_kmers = as.list(load_kmers, names = basename(load_kmers)) 
+|> tqdm 
+|> sapply(file -> load.expr(file))
+|> geneExpression::joinFeatures()
+;
+
+write.expr_matrix(load_kmers, 
+    file = box_matrix, 
+    id = "gene_context"
+);
