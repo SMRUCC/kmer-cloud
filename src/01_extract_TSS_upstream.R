0b5f28e232a622edcb9f3896c64194af7a7197c1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-18 00:50:14 +0800

    Update 01_extract_TSS_upstream.R

--- src/01_extract_TSS_upstream.R
+++ src/01_extract_TSS_upstream.R
@@ -1,6 +1,21 @@
 require(GCModeller);
 
+imports "GenBank" from "seqtoolkit";
+imports "genomics_context" from "seqtoolkit";
+imports "annotation.genomics" from "seqtoolkit";
+imports "bioseq.fasta" from "seqtoolkit";
+
 let genbank_repo = "D:\\datapool\\regprecise_genbank\\genomes";
-let extract_TSS_upstream = function(gb, len) {
-    
+let export_dir = relative_work("../data/tss_upstream");
+let extract_TSS_upstream = function(gbfile, len) {
+    let gb = read.genbank(gbfile);
+    let genes = gb |> genes_features();
+    let reg_box = gb |> TSS_upstream(genes, upstream_len = len);
+    let export_save = file.path(export_dir, `${basename(gbfile)}.fasta`);
+
+    write.fasta(reg_box, file = export_save);
+}
+
+for(let gb_asm in tqdm(list.files(genbank_repo, pattern = "*.gbff"))) {
+    gb_asm |> extract_TSS_upstream(len = 150);
 }
