1519be02cef3505a78203fb274db15bbb514260f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-18 01:26:31 +0800

    Update 04_embedding.R

--- src/04_embedding.R
+++ src/04_embedding.R
@@ -1,2 +1,12 @@
 require(GCModeller);
+require(umap);
 
+imports "geneExpression" from "phenotype_kit";
+
+let box_matrix = relative_work("../data/box_matrix.csv");
+let cloud_points = relative_work("../data/kmer_cloud.csv");
+let data = load.expr(box_matrix);
+let cloud = umap(data, dimension = 9, numberOfNeighbors = 64);
+cloud = as.data.frame(cloud$umap, labels = cloud$labels);
+
+write.csv(cloud, file = cloud_points);
