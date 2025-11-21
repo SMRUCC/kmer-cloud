d57c130eaa2fa9b7adb8dd5566ee3557da140cee
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-22 01:55:12 +0800

    make embedding and create plot

--- src/05_plot_cloud.R
+++ src/05_plot_cloud.R
@@ -1,10 +1,11 @@
-let cloud_points = relative_work("../data/kmer_cloud.csv");
+let cloud_points = ?"--matrix" || stop("umap embedding data matrix must be provided!");
+let saveRplot = ?"--save" || file.path(dirname(cloud_points), `${basename(cloud_points)}.png`);
 let cloud_data = read.csv(cloud_points, row.names = 1, check.names = FALSE);
-let tags = rownames(cloud_data) |> sapply(str -> .Internal::first(strsplit(str, "|",fixed =TRUE)));
+let tags = rownames(cloud_data) |> sapply(str -> .Internal::first(strsplit(str, " ",fixed =TRUE)));
 
 cloud_data[,"species"] = tags;
 
-bitmap(file = relative_work("../Rplot/kmers-cloud.png")) {
+bitmap(file = saveRplot) {
     plot(as.double(cloud_data$dimension_1), as.double(cloud_data$dimension_2), 
         class = cloud_data[,"species"], 
         colors = "paper", 
