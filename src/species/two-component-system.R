93591867c920a1259c4123871474c1ab0e549249
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-22 01:39:40 +0800

    filter two-component system data

--- src/species/two-component-system.R
+++ src/species/two-component-system.R
@@ -2,17 +2,15 @@ require(GCModeller);
 
 imports "geneExpression" from "phenotype_kit";
 
-# create a big matrix
-
-let data_dir = relative_work("../data/kmer_matrix");
-let box_matrix = relative_work("../data/box_matrix.csv");
+let data_dir = relative_work("../../data/kmer_matrix");
+let box_matrix = relative_work("./two-component-system.csv");
 let load_kmers = list.files(data_dir, pattern = "*.csv");
 
 print(basename(load_kmers));
 
 load_kmers = as.list(load_kmers, names = basename(load_kmers)) 
 |> tqdm 
-|> sapply(file -> load.expr(file))
+|> sapply(file -> load.expr(file) |> geneExpression::filter("two-component system"))
 |> geneExpression::joinFeatures(strict=FALSE)
 ;
 
